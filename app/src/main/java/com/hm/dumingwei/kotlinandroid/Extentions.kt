eb97c7bf1a1e872f3a59b04491bb6a615c66fef4
humanheima <humanheima@gmail.com>
2018-09-21 08:55:51 +0800

    Initial Commit

---
+++ app/src/main/java/com/hm/dumingwei/kotlinandroid/Extentions.kt
@@ -0,0 +1,82 @@
+package com.hm.dumingwei.kotlinandroid
+
+import android.app.Activity
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+
+
+/***
+ * 设置延迟时间的View扩展
+ * @param delay Long 延迟时间，默认600毫秒
+ * @return T
+ */
+fun <T : View> T.withTrigger(delay: Long = 600): T {
+    Log.d("extentions", "withTrigger")
+    triggerDelay = delay
+    return this
+}
+
+/***
+ * 点击事件的View扩展
+ * @param block: (T) -> Unit 函数
+ * @return Unit
+ */
+fun <T : View> T.click(block: (T) -> Unit) = setOnClickListener {
+
+    if (clickEnable()) {
+        block(it as T)
+    }
+}
+
+/***
+ * 带延迟过滤的点击事件View扩展
+ * @param delay Long 延迟时间，默认600毫秒
+ * @param block: (T) -> Unit 函数
+ * @return Unit
+ */
+fun <T : View> T.clickWithTrigger(time: Long = 600, block: (T) -> Unit) {
+    triggerDelay = time
+    setOnClickListener {
+        if (clickEnable()) {
+            block(it as T)
+        }
+    }
+}
+
+/**
+ * 利用了View的tag属性来保存我们的triggerDelay
+ */
+private var <T : View> T.triggerLastTime: Long
+    get() = if (getTag(1123460103) != null) getTag(1123460103) as Long else 0
+    set(value) {
+        setTag(1123460103, value)
+    }
+
+/**
+ * 利用了View的tag属性来保存我们的triggerDelay
+ */
+private var <T : View> T.triggerDelay: Long
+    get() = if (getTag(1123461123) != null) getTag(1123461123) as Long else -1
+    set(value) {
+        setTag(1123461123, value)
+    }
+
+private fun <T : View> T.clickEnable(): Boolean {
+    var flag = false
+    val currentClickTime = System.currentTimeMillis()
+    if (currentClickTime - triggerLastTime >= triggerDelay) {
+        flag = true
+    }
+    triggerLastTime = currentClickTime
+    return flag
+}
+
+
+fun Activity.toastSHortly(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Activity.toastSHortly(messageId: Int) {
+    Toast.makeText(this, getString(messageId), Toast.LENGTH_SHORT).show()
+}
