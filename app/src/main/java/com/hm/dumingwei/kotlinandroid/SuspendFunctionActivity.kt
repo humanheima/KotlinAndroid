a9bfa0ff8604171b32a3fe62be36e06a5a7d6468
forrest.du <forrest.du@ximalaya.com>
2021-11-13 14:31:45 +0800

    挂起函数完善。

--- app/src/main/java/com/hm/dumingwei/kotlinandroid/SuspendFunctionActivity.kt
+++ app/src/main/java/com/hm/dumingwei/kotlinandroid/SuspendFunctionActivity.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_suspend_function.*
 import kotlinx.coroutines.*
 
@@ -16,6 +18,12 @@ import kotlinx.coroutines.*
 class SuspendFunctionActivity : AppCompatActivity() {
 
     private val TAG: String = "SuspendFunctionActivity"
+
+    private lateinit var tvText1: TextView
+    private lateinit var tvText2: TextView
+
+    private lateinit var btnSuspend: Button
+    private lateinit var btnUnSuspend: Button
 
     companion object {
 
@@ -31,9 +39,46 @@ class SuspendFunctionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_suspend_function)
 
+        tvText1 = findViewById(R.id.tvText1)
+        tvText2 = findViewById(R.id.tvText2)
+
+        btnSuspend = findViewById(R.id.btnSuspend)
+        btnUnSuspend = findViewById(R.id.btnUnSuspend)
+
+        btnSuspend.setOnClickListener {
+            tvText1.text = getStringSuspend()
+            tvText2.text = "兴百姓苦，亡百姓苦"
+
+        }
+
+        btnUnSuspend.setOnClickListener {
+
+            scope.launch {
+                tvText1.text = getStringUnSuspend()
+            }
+            tvText2.text = "兴百姓苦，亡百姓苦"
+
+        }
         btnInvoke.setOnClickListener {
             invokeMethod()
         }
+    }
+
+    /**
+     * 模拟耗时操作，2秒后返回字符串
+     */
+    private fun getStringSuspend(): String {
+        Thread.sleep(2000)
+        return "峰峦如聚，波涛如怒，山河表里潼关路，望西都意踌躇，伤心秦汉经行处，宫阙万间都做了土。"
+    }
+
+    /**
+     * 模拟耗时操作，2秒后返回字符串。正确的的非阻塞，不阻塞当前线程，阻塞后台线程。
+     */
+    private suspend fun getStringUnSuspend(): String {
+        delay(2000)
+        return "峰峦如聚，波涛如怒，山河表里潼关路，望西都意踌躇，伤心秦汉经行处，宫阙万间都做了土。"
+
     }
 
     private fun invokeMethod() {
